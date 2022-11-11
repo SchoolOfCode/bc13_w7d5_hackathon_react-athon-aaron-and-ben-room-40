b5f304649a9a400ce0e00d4f8127e7ecbab7e946
BenPuusta <benpuusta@gmail.com>
2022-11-11 20:57:33 +0000

    ahahahaha

--- my-app/src/components/ListItem/index.js
+++ my-app/src/components/ListItem/index.js
@@ -1,9 +1,11 @@
 import DeleteButton from "../DeleteButton";
 import ItemText from "../ItemText";
+import "./index.css"
 
 function ListItem({todoText, removeItemFromList, index}) {
     return <div>
-        <ItemText todoText={todoText} />
+        <ItemText todoText={todoText} class = "list"
+        />
         <DeleteButton removeItemFromList={removeItemFromList} index={index} />   
         
     </div>
